abe4ba3bb75673b9dc69f0044c60dae6bfae78e8
Dryha Andrey <driga.and@gmail.com>
2023-05-27 17:59:23 +0300

    Made the display of a list of heroes filled with a superhero template

---
+++ frontend/src/components/header/header.js
@@ -0,0 +1,13 @@
+
+import './header.scss'
+
+function Header() {
+    return (
+        <header>
+            <h1>Superhero database</h1>
+            <button>Add Hero</button>
+        </header>
+    );
+}
+
+export default Header;
